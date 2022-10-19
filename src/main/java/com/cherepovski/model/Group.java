1516f86c45973e38d1d25812d57f447175a3a802
Illiami8 <87660266+Illiami8@users.noreply.github.com>
2022-10-19 22:52:47 +0300

    qualifier

--- src/main/java/com/cherepovski/model/Group.java
+++ src/main/java/com/cherepovski/model/Group.java
@@ -3,15 +3,17 @@ package com.cherepovski.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 import java.util.List;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Group implements Printing {
     private int id;
     private String name;
+
     private Teacher teacher;
     private List<Student> students;
 
@@ -20,7 +22,12 @@ public class Group implements Printing {
         this.name = name;
     }
 
-    //public Group(int id, String name,Teacher teacher){
+    @Autowired
+    @Qualifier("alex")
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+//public Group(int id, String name,Teacher teacher){
     //    this.id = id;
    //     this.name = name;
      //   this.teacher = teacher;
